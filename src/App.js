3f94f2d3b423e05d72d1b1d94b8492c4afae6218
manjunath00 <kadrimanjunath56@gmail.com>
2021-06-23 23:45:57 +0530

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./Header";
 import ProjectList from "./ProjectList";
